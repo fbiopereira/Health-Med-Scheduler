1e64865e7c1feff8aec2943d81c94d59ff56a235
alex jussiani junior <alexjunior_jussiani@hotmail.com>
2024-02-17 11:49:32 -0300

    ajuste controller

--- AloFInances/Controllers/PedidoController.cs
+++ AloFInances/Controllers/PedidoController.cs
@@ -1,38 +1,20 @@
-﻿using AloDoutor.Core.Messages;
-using AloDoutor.Core.Messages.Integration;
-using Azure.Core.Extensions;
-using MassTransit;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AloFinances.Api.Controllers
 {
     [ApiController]
-    [Route("/pedido")]
+    [Route("/financeiro")]
     public class PedidoController : ControllerBase
     {
-        private readonly IBus _bus; 
-        private readonly ILogger _logger;
 
-        public PedidoController(IBus bus, ILogger<string> logger)
+        public PedidoController()
         {
-            _bus = bus;
-            _logger = logger;
         }
 
-        [HttpPost("publish")]
-        public IActionResult Publish()
-        {
-            Task.Run(async () =>
-            {
-                for (int i = 0; i < 100000; i++) // Envie 100.000 mensagens para estressar a fila
-                {
-                    Guid id = Guid.NewGuid();
-                    await _bus.Publish(new RelatorioEvent(id));
-                    _logger.LogInformation($"{i} Mensagem publicada: {id}");
-                }
-            });
-
-            return Ok();
+        [HttpPost("status-api")]
+        public IActionResult StatusAPI()
+        {  
+            return Ok("OK");
         }               
     }
 }

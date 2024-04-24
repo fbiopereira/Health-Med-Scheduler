b7076b678fbff3b1e9fb330c51022108b2576a3a
RichardKT88 <kendy_t88@hotmail.com>
2024-04-24 15:22:10 -0300

    Removendo namespaces que não são usados.

--- AloDoutor.Application.UnitTests/Features/Medicos/Commands/AdicionarMedicoCommandHandlerTests.cs
+++ AloDoutor.Application.UnitTests/Features/Medicos/Commands/AdicionarMedicoCommandHandlerTests.cs
@@ -2,7 +2,6 @@
 using AloDoutor.Application.Interfaces;
 using AloDoutor.Application.MappingProfiles;
 using AloDoutor.Application.UnitTests.Mocks;
-using AloDoutor.Domain.Entity;
 using AutoMapper;
 using Moq;
 using Shouldly;
@@ -31,7 +30,7 @@ namespace AloDoutor.Application.UnitTests.Features.Medicos.Commands
         {
             var handler = new AdicionarMedicoCommandHandler(_mapper, _mockMedicoRepo.Object);
 
-            await handler.Handle(new AdicionarMedicoCommand() { Cpf = "82841218082", Nome = "Medico 1", Cep = "11111111111", Telefone = "2222-2222", Endereco = "Rua Fake", Estado = "Fake", Crm = "AA-12345" }, CancellationToken.None);     
+            await handler.Handle(new AdicionarMedicoCommand() { Cpf = "82841218082", Nome = "Medico 1", Cep = "11111111111", Telefone = "2222-2222", Endereco = "Rua Fake", Estado = "Fake", Crm = "AA-12345" }, CancellationToken.None);
 
             var medicos = await _mockMedicoRepo.Object.ObterTodos();
             medicos.Count.ShouldBe(4);

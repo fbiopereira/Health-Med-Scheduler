ac3cc7b0bd0ea84477848a2873cc02794702c8b8
alex jussiani junior <alexjunior.jussiani@gmail.com>
2024-04-28 23:16:33 -0300

    ajuste

--- AloDoutor.Application/Features/Medicos/Queries/ObterTodosMedicos/ObterMedicosQuery.cs
+++ AloDoutor.Application/Features/Medicos/Queries/ObterTodosMedicos/ObterMedicosQuery.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace AloDoutor.Application.Features.Medicos.Queries.ObterTodosMedicos
 {
-    public record ObterPacinetesQuery : IRequest<IEnumerable<MedicoViewModel>>;
+    public record ObterMedicosQuery : IRequest<IEnumerable<MedicoViewModel>>;
     public record ObterMedicoPorIdQuery(Guid idMedico) : IRequest<MedicoViewModel>;
     public record ObterEspecialidadePorIdMedicoQuery(Guid idMedico) : IRequest<MedicoViewModel>;
     public record ObterAgendamentoMedicoPorIdMedicoQuery(Guid idMedico) : IRequest<MedicoViewModel>;
